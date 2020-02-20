e66a2ae9583811945583b8dade45365411341c36
Saheed Oladosu <38882296+kodeGenius@users.noreply.github.com>
2020-02-20 20:51:23 +0100

    updated

--- src/index.js
+++ src/index.js
@@ -6,6 +6,5 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'tachyons';
 
-//DOM setup
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
